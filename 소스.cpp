c5814851392234f0754b81d72c98674ee5db7d4b
brent oh <brent912@naver.com>
2022-04-08 10:40:36 +0900

    소스.cpp update

--- 소스.cpp
+++ 소스.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 
+//9를 10으로 수정했음 : 2019102197오세헌
+
 int main()
 {
-	for (int i = 1; i < 9; i++)
+	for (int i = 1; i < 10; i++)
 	{
 		for (int j = 1; j < 10; j++)
 			cout << i << " * " << j << " = " << i * j << endl;
